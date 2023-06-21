e1cc16a37c7c07ccfd07001def235885ef427eed
yp8866 <122187543+yp8866@users.noreply.github.com>
2023-06-21 21:41:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+const port=3000;
+
+app.use(express.static(path.join(__dirname,"Public")))
+// app.get('/services.html',(req,res)=>{
+//     res.sendFile(path.join(__dirname,"/Public/services.html"));
+// })
+app.listen(port,()=>{
+    console.log(`The server running `);
+})
